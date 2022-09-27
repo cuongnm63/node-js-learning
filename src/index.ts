26d4828a77ce55e30df52e843312f6dbb1b66d47
Cuong Nguyen <cuong_nguyen1@epam.com>
2022-09-27 07:55:06 +0700

    Connected to DB

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,21 @@ dotenv.config();
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
+const db = require("./models/index");
+
+db.sequelize.sync()
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err: {message: string}) => {
+        console.log("Failed to sync db: " + err.message);
+    })
+
+app.get("/", (req: any, res: any) => {
     res.json({ message: "Welcome to Cuong Test" });
-});
+})
 
 const PORT = process.env.APP_PORT || 1212;
 app.listen(PORT, () => {
